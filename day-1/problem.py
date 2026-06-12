83b45efe7d16319c2eda74672dd2329fa6b9256e
Daniel Fadehan <fadehandaniel2006@gmail.com>
2026-06-12 13:56:34 +0100

    init

---
+++ day-1/problem.py
@@ -0,0 +1,26 @@
+def backspaceCompare(self, s: str, t: str) -> bool:
+    list_s = []  s = "ab#c" -> [a, c]
+    list_t = [] -> (s+t) -> O(n)
+    
+    
+    for char in s:
+        
+        if list_s and char == '#':
+            list_s.pop()
+        elif:
+          list_s.append(char)
+
+    for char in t:
+      if list_t and char == '#':
+            list_t.pop()
+        elif:
+          list_t.append(char)
+
+return list_s == list_t -> [a, c] == [a, c] 
+            
+
+
+def climbStairs(self, n):
+    if n == 0 or n == 1:
+        return 1
+    return climbStairs(n - 1)  +  climbStairs(n-2)
