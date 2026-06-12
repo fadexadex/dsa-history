e41ec93b85bb2317dece1305c8b89985b0b43795
Daniel Fadehan <fadehandaniel2006@gmail.com>
2026-06-12 13:57:54 +0100

    edits

--- day-1/problem.py
+++ day-1/problem.py
@@ -1,3 +1,5 @@
+# backspaceCompare inefficient solution
+
 def backspaceCompare(self, s: str, t: str) -> bool:
     list_s = []  s = "ab#c" -> [a, c]
     list_t = [] -> (s+t) -> O(n)
@@ -19,8 +21,18 @@ def backspaceCompare(self, s: str, t: str) -> bool:
 return list_s == list_t -> [a, c] == [a, c] 
             
 
-
+# climbStairs (dp solution)
 def climbStairs(self, n):
     if n == 0 or n == 1:
         return 1
     return climbStairs(n - 1)  +  climbStairs(n-2)
+
+# climbStairs (memoization solution)
+def climbStairs(self, n):
+    memo = {}
+    if n in memo:
+        return memo[n]
+    if n == 0 or n == 1:
+        return 1
+    memo[n] = climbStairs(n - 1)  +  climbStairs(n-2)
+    return memo[n]
